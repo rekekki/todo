578b49abf718b551ac4b0ece01bf4fdbca678be5
rekekki <r2101k@pm.me>
2021-04-14 23:03:55 +0300

    created another component to use in App. Testing importing and exporting. Wrapped elements in div tag, since return returns only one thing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import TodoList from "./components/TodoList"
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+    <h1>Hello World</h1>
+    <TodoList />
     </div>
   )
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const TodoList = () => {
+    return (
+        <div>
+            <h2>Hello List</h2>
+        </div>
+    )
+}
+
+export default TodoList
