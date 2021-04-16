b49a1c179af470b2d88700e339f8d4ad76f0963a
rekekki <r2101k@pm.me>
2021-04-16 14:56:00 +0300

    caught a key error in console. Solved it with adding a key unique to the array using it.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@ import Todo from "./Todo"
 const TodoList = ({ todos }) => {
     return (
         todos.map(todo => {
-            return <Todo todo={todo} />
+            return <Todo key={todo} todo={todo} />
         })
     )
 }
