da52449f757b307b6c431e7a91f6bd87b69ad2bd
rekekki <r2101k@pm.me>
2021-05-16 00:09:51 +0300

    created a label for including a chechkbox

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,10 @@ import React from "react"
 const Todo = ({ todo }) => {
     return (
         <div>
+            <label>
+                <input type="checkbox" checked={todo.complete} />
             {todo.name}
+            </label>
         </div>
     )
 }
